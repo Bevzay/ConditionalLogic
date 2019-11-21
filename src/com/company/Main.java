829523ee6ea03501d770efc738e70437ee894c81
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2019-11-21 14:58:50 +1300

    if statement and code block

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        boolean isAlien = false;
+
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
     }
 }
